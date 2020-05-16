149a790a85ec79d6016c69a6ae389b524f8f821c
poleshe <polestecha14@gmail.com>
2020-05-16 17:58:52 +0200

    purge corrutped git files... bummer

--- test.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
-driver = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub', desired_capabilities=DesiredCapabilities.CHROME)
-
-driver.get('https://www.reddit.com')
-screenshot = driver.save_screenshot('test1.png')
-driver.quit()
